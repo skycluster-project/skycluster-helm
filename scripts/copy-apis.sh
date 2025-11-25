b7ce0e7fc7e33cc9534b715ea2c29575ec3c1a18
Ehsan E. <eh.etesami@gmail.com>
2025-11-25 17:47:48 +0000

    Add definitions to crds folder

--- scripts/copy-apis.sh
+++ scripts/copy-apis.sh
@@ -2,7 +2,8 @@
 
 SRC_ROOT="/home/ubuntu/skycluster-project/skycluster-compositions"
 SRC_FILE="$1"
-DEST_ROOT="../crds/templates"
+TMPL_ROOT="../crds/templates"
+CRDS_ROOT="../crds/crds"
 
 if [[ -z "$SRC_FILE" ]]; then
     echo "Usage: $0 <file_path>"
@@ -18,8 +19,15 @@ fi
 SRC_FILE=${SRC_FILE#./}
 FULL_PATH="${SRC_ROOT}/${SRC_FILE}"
 
-# Full destination path
-DEST_PATH="$DEST_ROOT/$SRC_FILE"
+# Determine destination based on filename
+if [[ "$SRC_FILE" == *"composition.yaml"* ]]; then
+    DEST_PATH="$TMPL_ROOT/$SRC_FILE"
+elif [[ "$SRC_FILE" == *"definition.yaml"* ]]; then
+    DEST_PATH="$CRDS_ROOT/$SRC_FILE"
+else
+    echo "Error: Filename must contain either 'compositions.yaml' or 'definition.yaml'"
+    exit 1
+fi
 
 # # Create the target directory
 mkdir -p "$(dirname "$DEST_PATH")"
